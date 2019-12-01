f9d25a9e32d14486fb82a554b294a3aac42dd7b4
rahulcoding <45851454+rahulcoding@users.noreply.github.com>
2019-12-02 00:38:46 +0530

    Add files via upload

--- deployment/app.py
+++ deployment/app.py
@@ -2,12 +2,14 @@ from flask import Flask,request,render_template
 app = Flask(__name__)
 
 from commons import baki_ka_chod
-
+from dotdot import dot
 
 @app.route('/',methods = ['GET','POST'])
+@app.route('/index')
 def hello():
 	if request.method == 'GET':
 		return render_template('index.html',value = '1000')
+		
 	if request.method == 'POST':
 		if 'file' not in request.files:
 			print('file not uploaded')
@@ -15,7 +17,8 @@ def hello():
 		file = request.files['file']
 		image = file.read()
 		predictions = baki_ka_chod(image_bytes=image)
-		return render_template('result.html',xray = predictions)
+		path = dot(image_bytes=image,prediction=predictions)
+		return render_template('result.html',xray = predictions,image_path=path)
 
 
 if __name__ == '__main__':
